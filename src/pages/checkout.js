1919f522ea785e2176c9c0320c0f1112a09fa35d
hamrosh <hamroshroy@gmail.com>
2019-09-26 15:05:16 +0530

    File Name Changed in import

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Checkout from "../components/checkout/checkout"
+import Checkout from "../components/checkout/Checkout"
 import Layout from "../components/layout/Layout"
 
 const checkout = () => {
