1c4dc4d443f810d316070c06ae92c83ffcc8fc78
hamrosh <hamroshroy@gmail.com>
2019-10-05 19:46:22 +0530

    changed nprogess config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,12 +32,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-nprogress`,
+      resolve: "gatsby-plugin-nprogress",
       options: {
-        // Setting a color is optional.
-        color: `tomato`,
-        // Disable the loading spinner.
         showSpinner: false,
+        trickle: false,
+        minimum: 0.4,
       },
     },
   ],
